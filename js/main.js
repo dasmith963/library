294d99833bbe58c007788f9a527fd39bdce919d0
Devon <Devon.smith017@gmail.com>
2023-06-27 12:24:09 -0500

    Create constructor to instantiate book objects.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const myLibrary = [];
+
+function Book(title, author, totalPages, status){
+  this.title = title;
+  this.author = author;
+  this.totalPages = totalPages;
+  this.status = status;
+}
+
+
