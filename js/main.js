45fad5f9d27af750e37903123259c4afdb9102fa
Devon <Devon.smith017@gmail.com>
2023-06-27 20:36:35 -0500

    Create function to display book cards on the page.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const bookCards = document.querySelector(".book-cards");
 const myLibrary = [];
 
 function Book(title, author, totalPages, status){
@@ -9,4 +10,23 @@ function Book(title, author, totalPages, status){
 
 function addBookToLibrary(title, author, totalPages, status){
   myLibrary.push(new Book(title, author, totalPages, status));
+}
+
+function createBookCard(){
+  myLibrary.forEach(book => {
+    const card = document.createElement("div");
+    const bookTitle = document.createElement("h2");
+    const bookAuthor = document.createElement("p");
+    const bookLength = document.createElement("p");
+
+    card.className = "card";
+    bookTitle.textContent = `${book.title}`;
+    bookAuthor.textContent = `by ${book.author}`;
+    bookLength.textContent = `Pages: ${book.totalPages}`;
+
+    card.appendChild(bookTitle);
+    card.appendChild(bookAuthor);
+    card.appendChild(bookLength);
+    bookCards.appendChild(card);
+  })
 }
