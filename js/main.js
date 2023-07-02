015cc00a3dde8bac4c4d1a20663d763217f192c4
Devon <Devon.smith017@gmail.com>
2023-07-01 22:48:26 -0500

    Add functions to open and close form modal.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
+const addBookBtn = document.querySelector(".add-book-btn");
 const bookCards = document.querySelector(".book-cards");
+const formModal = document.querySelector(".form-modal");
+const closeBtn = document.querySelector(".close-btn");
+const overlay = document.querySelector(".overlay");
+const form = document.querySelector("form");
 const myLibrary = [];
 
-function Book(title, author, totalPages, status){
+addBookBtn.addEventListener("click", openFormModal);
+closeBtn.addEventListener("click", closeFormModal);
+overlay.addEventListener("click", closeFormModal);
+
+function Book(title, author, totalPages, status) {
   this.title = title;
   this.author = author;
   this.totalPages = totalPages;
   this.status = status;
 }
 
-function addBookToLibrary(title, author, totalPages, status){
+function addBookToLibrary(title, author, totalPages, status) {
   myLibrary.push(new Book(title, author, totalPages, status));
 }
 
-function createBookCard(){
+function createBookCard() {
   myLibrary.forEach(book => {
     const card = document.createElement("div");
     const bookTitle = document.createElement("h2");
@@ -29,4 +38,15 @@ function createBookCard(){
     card.appendChild(bookLength);
     bookCards.appendChild(card);
   })
+}
+
+function openFormModal() {
+  formModal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function closeFormModal() {
+  formModal.classList.add("hidden");
+  overlay.classList.add("hidden");
+  form.reset();
 }
